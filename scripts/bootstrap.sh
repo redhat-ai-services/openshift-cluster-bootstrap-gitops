1a902ab0503cbfb79cb9bea607a6adf8558828f3
Trevor Royer <troyer@redhat.com>
2023-02-15 09:30:15 -0600

    update ns to use var

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,16 +22,16 @@ install_gitops(){
   # https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#wait
 
   echo "Waiting for deployment of the gitops-operator-controller-manager to begin..."
-  until oc get deployment gitops-operator-controller-manager -n openshift-operators
+  until oc get deployment gitops-operator-controller-manager -n ${ARGO_NS}
   do
     sleep 5
   done
 
   echo "Waiting for gitops-operator-controller-manager to start..."
-  oc wait --for=condition=Available deployment/gitops-operator-controller-manager -n openshift-operators --timeout=${TIMEOUT_SECONDS}s
+  oc wait --for=condition=Available deployment/gitops-operator-controller-manager -n ${ARGO_NS} --timeout=${TIMEOUT_SECONDS}s
 
-  echo "Waiting for openshift-gitops namespace to be created..."
-  oc wait --for=jsonpath='{.status.phase}'=Active namespace/openshift-gitops --timeout=${TIMEOUT_SECONDS}s
+  echo "Waiting for ${ARGO_NS} namespace to be created..."
+  oc wait --for=jsonpath='{.status.phase}'=Active namespace/${ARGO_NS} --timeout=${TIMEOUT_SECONDS}s
 
   echo "Waiting for deployments to start..."
   oc wait --for=condition=Available deployment/cluster -n ${ARGO_NS} --timeout=${TIMEOUT_SECONDS}s
